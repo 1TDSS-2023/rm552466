d12972da8c3ba1d28a570f6cc0860649d3e7efd7
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2023-04-28 20:58:51 -0300

    Adicionando JS e img

---
+++ js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+// console.log("Olá Mundo!")
+
+
+// var nr1 = "10";
+// var nr2 = "5";
+
+// console.log(parseInt(nr1) + parseInt(nr2))
+
+var nome1 = "Dubas";
+let nome2 = "Serbatinho";
+const nome3 = "Julia";
+
+console.log(nome)
+if (nome1 != "") {
+    var nome1 = "Julia";
+}
+
+
+console.log(nome1);
